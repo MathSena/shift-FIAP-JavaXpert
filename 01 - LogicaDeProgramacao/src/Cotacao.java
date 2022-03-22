0fdce7a7360cc7ca4b37552bd75fcc3e3e4a0c81
MathSena <MathSena07@hotmail.com>
2022-03-22 20:35:01 -0300

    Exercícios aula 01

---
+++ 01 - LogicaDeProgramacao/src/Cotacao.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Cotacao {
+
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Digite o valor recebido em Dólar: ");
+        double valorEmDolar = leitor.nextDouble();
+        double cotacao = 4.91;
+        double valorEmReais = valorEmDolar*cotacao;
+
+        System.out.println("Você recebeu R$: " + String.format("% .2f",valorEmReais) + " em reais!" );
+
+    }
+}
---
+++ 01 - LogicaDeProgramacao/src/Quilometragem.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Quilometragem {
+
+
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Digite a quilometragem atual do painel: ");
+        double kmAtual = leitor.nextDouble();
+        System.out.println("Digite a quilometragem ao fim da sua viagem: ");
+        double kmAposViagem = leitor.nextDouble();
+        System.out.println("Quantos litros foram reabastecidos: ");
+        double litros = leitor.nextDouble();
+
+        double litroPorKm = (kmAposViagem - kmAtual)/litros;
+
+        System.out.println("O carro gastou aproximadamente: " + litroPorKm + " litros por quilometro");
+
+        leitor.close();
+    }
+}
